ca0204e6b172ac4ff49acc7834f18be044696590
Bauyrzhan Abdi <bauyrzhan.abdi@nu.edu.kz>
2023-02-22 17:26:33 +0600

    Revert "Merge pull request #1 from bauyrzhanabdi/feature/add-login-label"
    This reverts commit fd0b866577cf3c1d97fd1b4a67c5bc24aae37f6c, reversing
    changes made to f6cad47b3493bb8b421ae7d4a3805bf5626faad5.

--- iOS7-HW11/ViewController.swift
+++ iOS7-HW11/ViewController.swift
@@ -1,50 +1,19 @@
+//
+//  ViewController.swift
+//  iOS7-HW11
+//
+//  Created by Bauyrzhan Abdi on 22.02.2023.
+//
+
 import UIKit
 
 class ViewController: UIViewController {
-    // MARK: - Outlets
-    
-    private lazy var label: UILabel = createLabel(
-        text: "Login",
-        allignment: .center,
-        color: .white,
-        fontSize: 25,
-        fontWeight: 1
-    )
-    
-    
-    // MARK: - Lifecycle
-    
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .systemBlue
-        setupHierarchy()
-        setupLayout()
+        // Do any additional setup after loading the view.
     }
-    
-    // MARK: - Setup
-    
-    private func setupHierarchy() {
-        view.addSubview(label)
-    }
-    
-    private func setupLayout() {
-        NSLayoutConstraint.activate([
-            label.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 75),
-            label.centerXAnchor.constraint(equalTo: view.centerXAnchor)
-        ])
-    }
-    
-    // MARK: - Methods
-    
-    private func createLabel(text: String, allignment: NSTextAlignment, color: UIColor, fontSize: CGFloat, fontWeight: CGFloat) -> UILabel {
-        let label = UILabel()
-        label.text = text
-        label.textAlignment = allignment
-        label.textColor = color
-        label.font = UIFont.systemFont(ofSize: fontSize, weight: UIFont.Weight(fontWeight))
-        label.translatesAutoresizingMaskIntoConstraints = false
-        return label
-    }
+
 
 }
 
