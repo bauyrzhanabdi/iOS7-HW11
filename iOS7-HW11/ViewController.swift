4587f0b313b4675b08614cd281429da0f024628a
Bauyrzhan Abdi <bauyrzhan.abdi@nu.edu.kz>
2023-02-22 17:28:28 +0600

    add login label

--- iOS7-HW11/ViewController.swift
+++ iOS7-HW11/ViewController.swift
@@ -9,11 +9,50 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
-    }
-
+    // MARK: - Outlets
+     
+     private lazy var label: UILabel = createLabel(
+         text: "Login",
+         allignment: .center,
+         color: .white,
+         fontSize: 25,
+         fontWeight: 1
+     )
+     
+     
+     // MARK: - Lifecycle
+     
+     override func viewDidLoad() {
+         super.viewDidLoad()
+         view.backgroundColor = .systemBlue
+         setupHierarchy()
+         setupLayout()
+     }
+     
+     // MARK: - Setup
+     
+     private func setupHierarchy() {
+         view.addSubview(label)
+     }
+     
+     private func setupLayout() {
+         NSLayoutConstraint.activate([
+             label.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 75),
+             label.centerXAnchor.constraint(equalTo: view.centerXAnchor)
+         ])
+     }
+     
+     // MARK: - Methods
+     
+     private func createLabel(text: String, allignment: NSTextAlignment, color: UIColor, fontSize: CGFloat, fontWeight: CGFloat) -> UILabel {
+         let label = UILabel()
+         label.text = text
+         label.textAlignment = allignment
+         label.textColor = color
+         label.font = UIFont.systemFont(ofSize: fontSize, weight: UIFont.Weight(fontWeight))
+         label.translatesAutoresizingMaskIntoConstraints = false
+         return label
+     }
 
 }
 
